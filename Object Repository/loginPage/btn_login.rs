090b1b46a0b90f433a3f09f3ec07f4c0e0fb3683
izzazainf <izzazain25@gmail.com>
2023-01-18 09:09:43 +0700

    create login for positive cases

---
+++ Object Repository/loginPage/btn_login.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_login</name>
+   <tag></tag>
+   <elementGuidId>954f680d-49e3-458e-84a2-bddb6bbe2aa7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'w-36 mobile:w-24 mobile:py-1 flex items-center justify-center desktop:py-3 py-1 w-full desktop:text-lg mobile:w-full bg-blue-4 text-blue-1 text-button ']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>w-36 mobile:w-24 mobile:py-1 flex items-center justify-center desktop:py-3 py-1 w-full desktop:text-lg mobile:w-full bg-blue-4 text-blue-1 text-button </value>
+      <webElementGuid>fd222e5a-1bff-49c0-8afe-c1c7209997d3</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
